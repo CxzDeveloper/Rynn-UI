c2c25219ea86eed57d32bc47fa15c96a30b05bcd
CxzDeveloper <krisnaokeh@gmail.com>
2025-03-25 19:52:54 +0700

    Update randomhadits.js

--- src/api/muslim/randomhadits.js
+++ src/api/muslim/randomhadits.js
@@ -1,1 +1,24 @@
+module.exports = function (app) {
+    const axios = require("axios");
 
+    app.get("/random/hadits", async (req, res) => {
+        try {
+            const { data } = await axios.get("https://api.myquran.com/v2/hadits/arbain/semua");
+            const datanya = data.data;
+            const randomData = datanya[Math.floor(Math.random() * datanya.length)];
+
+            res.json({
+                status: true,
+                result: {
+                    sumber: "Scraper Hadits Arbain",
+                    nomor: randomData.no,
+                    judul: randomData.judul,
+                    teks_arab: randomData.arab,
+                    terjemahan: randomData.indo
+                }
+            });
+        } catch (error) {
+            res.status(500).json({ status: false, error: "Gagal mengambil data" });
+        }
+    });
+};
